42c37cff0cb9f2a8e370d8c5dfa76488cd628681
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-03-12 21:38:21 +0500

    setting main urls, views and templates

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+    path('',views.index,name='index'),
+    path('charge',views.charge,name='charge'),
+    path('success/',views.success,name='success'),
     
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'main/index.html')
+
+def charge(request):
+    return 
+
+def success(request):
+    return render(request,'main/success.html')
