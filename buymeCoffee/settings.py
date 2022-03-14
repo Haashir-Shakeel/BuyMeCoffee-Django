2e61ef4dd0f33e1e51fb8f1664a90007f70175ca
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-03-15 03:09:09 +0500

    forcing to https whenever live

--- buymeCoffee/settings.py
+++ buymeCoffee/settings.py
@@ -138,4 +138,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 if os.getcwd() == '/app':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
+    SECURE_SSL_REDIRECT = True
     DEBUG = False
