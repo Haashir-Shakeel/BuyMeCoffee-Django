9d7a91c0d9bec661877e10a897b27e0a0099d30c
HaashirBinShakeel <56473466+HaashirBinShakeel@users.noreply.github.com>
2022-03-14 16:52:36 +0500

    views.charge working on dummy amount

--- main/views.py
+++ main/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
 
+from django.urls import reverse
+from django.shortcuts import redirect, render
+import stripe
+stripe.api_key = "sk_test_51Kd8gGAWQSg3LicKwKJSs4VgrvNLysUdcORVZgNNSv8rbJunT8SSJfyPLHjjGpbiwR67ICfqjGAIazNYwS0P7OaL00uM17LbCj"
 
 # Create your views here.
 def index(request):
     return render(request,'main/index.html')
 
 def charge(request):
-    return
+    amount = 5
+    if request.method == 'POST':
+        print('Data:',request.POST)
+         
+    return redirect(reverse('success', args=[amount]))
 
 def success(request, args):
     amount = args
